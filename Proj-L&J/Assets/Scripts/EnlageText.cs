aae5d336519d689718178c2788f398b0ea4508b0
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2019-09-02 13:28:13 +0200

    Fuck 7

---
+++ Proj-L&J/Assets/Scripts/EnlageText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnlageText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ChangeScale());
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            GameObject.Find("TransitionSprite").GetComponent<TransitionScript>().Nibba();
+        }
+    }
+
+    public IEnumerator ChangeScale()
+    {
+        for (int i = 0; i < 100000000; i++)
+        {
+            transform.localScale += new Vector3(0.2F, 0.2F, 0);
+            yield return new WaitForSeconds(0.4f);
+            transform.localScale += new Vector3(-0.2F, -0.2F, 0);
+            yield return new WaitForSeconds(0.4f);
+        }
+    }
+}
---
+++ Proj-L&J/Assets/Scripts/TransitionScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionScript : MonoBehaviour
+{
+
+    public void Nibba()
+    {
+        StartCoroutine(ChangeScales());
+    }
+    public IEnumerator ChangeScales()
+    {
+        for (int i = 0; i < 14; i++)
+        {
+            transform.localScale += new Vector3(0.09F, 0.09F, 0);
+            yield return new WaitForSeconds(0.1f);
+
+        }
+    }
+}
