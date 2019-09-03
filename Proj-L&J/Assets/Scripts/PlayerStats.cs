291223b71c3594bc20d1eccdd0206722c3bbe5c9
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2019-09-03 20:05:13 +0200

    Update PlayerStats.cs

--- Proj-L&J/Assets/Scripts/PlayerStats.cs
+++ Proj-L&J/Assets/Scripts/PlayerStats.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     public int playerHP;
+    private bool CanTakeDamage;
 
     // Start is called before the first frame update
     void Start()
     {
+        CanTakeDamage = true;
         playerHP = 3;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "BulletEnemy")
+        if(collision.gameObject.tag == "BulletEnemy" && CanTakeDamage == true)
         {
+            CanTakeDamage = false;
             playerHP -= 1;
         }
+    }
+
+    private IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(2);
+        CanTakeDamage = true;
     }
 }
