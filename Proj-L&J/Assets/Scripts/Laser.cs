084b82841c42fb52de3ad51100c1dc6ee2fd752a
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2019-09-03 19:15:18 +0200

    Fuck 31

---
+++ Proj-L&J/Assets/Scripts/Laser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("delete", 0.5F);
+    }
+
+    private void delete()
+    {
+        Destroy(gameObject);
+    }
+}
